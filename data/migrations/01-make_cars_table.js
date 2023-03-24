114c5172978b816bfc1e8190f4766ba31f172839
Blake Alderink <blake.alderink@gmail.com>
2023-03-24 13:48:34 -0700

    added the seeds and the migrations for the database

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,15 @@
 exports.up = function (knex) {
+  return knex.schema.createTable('cars', table => {
+    table.increments();
+
+    table.string('car_id', 20).notNullable().unique()
+
+    table.string('make', 128).notNullable()
+  })
   // DO YOUR MAGIC
 };
 
 exports.down = function (knex) {
+  return knex.schema.dropTableIfExists('cars')
   // DO YOUR MAGIC
 };
--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,23 @@
 // STRETCH
+const cars = [
+
+{
+    car_id: '1',
+    make: 'Toyota'
+}, 
+
+{
+    car_id: '2',
+    make: 'Ford'
+}, {
+    car_id: '3',
+    make: 'Tesla'
+}
+
+]
+
+exports.seed = function (knex) {
+    return knex('cars').truncate().then(() => {
+        return knex('cars').insert(cars)
+    })
+}
